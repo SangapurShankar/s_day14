6cb753491ed89f2e685580c0dc561e37710404e4
Good Luck <Roshansangapur@gmail.com>
2022-02-25 00:00:26 +0530

    Day 14 committed

---
+++ s_Day14/src/s_Day14/ComparetoDemo.java
@@ -0,0 +1,13 @@
+package s_Day14;
+
+public class ComparetoDemo {
+
+	public static void main(String[] args) {
+		String x="a";
+		String y="c";
+		System.out.println("Result :"+ x.compareTo(y));
+
+
+	}
+
+}
